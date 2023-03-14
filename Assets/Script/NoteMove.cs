8fd2c7d99c96937e0266269def98b6dd7feb3049
MagicPonzu <114977533+MagicPonzu@users.noreply.github.com>
2023-03-15 03:06:46 +0900

    ノーツ当たり判定追加

--- Assets/Script/NoteMove.cs
+++ Assets/Script/NoteMove.cs
@@ -2,29 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Notes : MonoBehaviour
+public class NoteMove : MonoBehaviour
 {
     [SerializeField]float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0.0003f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.localPosition -= new Vector3(0f, speed, 0f);
-
-        // transformを取得
-        Transform myTransform = this.transform;
- 
-        // ローカル座標を基準に、座標を取得
-        Vector3 localPos = myTransform.position;
-        float z = localPos.z;
-
-        if(z < -0.003f) {
-            Destroy(this.gameObject);
+        if(transform.localPosition.y <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
