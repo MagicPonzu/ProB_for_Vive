5d621a97575209d2087aaae0f4bf0b5db85ad235
MagicPonzu <114977533+MagicPonzu@users.noreply.github.com>
2023-03-15 18:29:07 +0900

    Update NoteMove.cs

--- Assets/Script/NoteMove.cs
+++ Assets/Script/NoteMove.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class NoteMove : MonoBehaviour
 {
     [SerializeField]float speed;
+    int x = 0;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.0003f;
+        speed = 0.0001f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition -= new Vector3(0f, speed, 0f);
-        if(transform.localPosition.y <= 0)
+        if(Input.GetKey(KeyCode.Return)) //エンターキー入力
         {
-            Destroy(gameObject);
+            x = 1;
+        }
+        if(Input.GetKey(KeyCode.Space)) //スペースキー入力
+        {
+            x = 0;
+        }
+
+        while(x == 1) {
+            transform.localPosition -= new Vector3(0f, speed, 0f);
+            if(transform.localPosition.y <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
